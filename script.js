074028c03fed99d0084a16b92fdf90e30270fca2
Devin Payton <61727482+dpayton92@users.noreply.github.com>
2020-05-23 00:51:53 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ generateBtn.innerHTML
 // Write password to the #password input
 function writePassword() {
   console.log(1);
-  var password = generatePassword();
+  var password = "";
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
   var passwordText = document.querySelector("#password");
+  for (i = 1; i <= 8; i++) {
+    var char = Math.floor(Math.random()
+      * str.length + 1);
+
+    password += str.charAt(char)
+    console.log(password)
+    
+  }
 
   passwordText.value = password;
 
@@ -18,6 +27,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
- 
+
 
 
