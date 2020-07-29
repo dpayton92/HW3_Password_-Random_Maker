7cc29a40300b840fd83f155f5cb880f7d851d7f2
Devin Payton <61727482+dpayton92@users.noreply.github.com>
2020-07-29 00:01:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-generateBtn.innerHTML
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!$^&*-=+_?";
 
-// Write password to the #password input
-function writePassword() {
-  console.log(1);
-  var password = "";
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-  var passwordText = document.querySelector("#password");
-  for (i = 1; i <= 8; i++) {
-    var char = Math.floor(Math.random()
-      * str.length + 1);
 
-    password += str.charAt(char)
-    console.log(password)
+
+var newPassWd = [];
+
+
+
+
+
+
+var pWlenght =  parseInt(prompt("How many characters do you want to be in your password?"));
+
+
+if (pWlenght < 8) {
+    alert("All passwords must be between 8-128 characters.");
+    prompt("How many characters do you want to be in your password?");
     
-  }
+   }
+   else if (pWlenght > 8 ); {
 
-  passwordText.value = password;
+
+   }
+upperC = confirm("Click ok to confirm including upper case letters."); 
+lowerC = confirm("Click ok to confirm including lower case letters."); 
+numericV = confirm("Click ok to confirm including numeric values."); 
+specialC = confirm("Click ok to confirm including special characters."); 
+
+
+if (upperC === true) {
+   (newPassWd.push(caps));
+}
+ if (lowerC === true) {
+    (newPassWd.push(lower));
+ }
+if (numericV === true) {
+    (newPassWd.push(numeric));
+ }
+if (specialC = true) {
+    (newPassWd.push(special));
+ }
+
+
+var newPWstr = newPassWd.join("");
+ 
+var password = "";
+
+
+for (var i = 0; i < pWlenght; i++) {
+var genChr = newPWstr.charAt(Math.floor(Math.random() * newPWstr.length));
+password = password.concat(genChr);
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+alert("click generate password below to receive your new password");
 
 
-
-
-
-
-
+function generatePW() {
+   document.getElementById("passWD-text").textContent = password;
+}
+function copy() {
+     document.getElementById("passWD-text").select();
+     document.execCommand("CopyPW");
+     alert("Your password has been copied to clipbaord.")
+  }
+  
+  
+  console.log(password);
+  console.log(pWlenght);
+  console.log(upperC);
+  console.log(lowerC);
+  console.log(numericV);
+  console.log(specialC);
+  console.log(newPassWd);
+  console.log(newPWstr);
+  console.log(genChr);
